226ee8f601257bf2faed1439517385dbd87431f7
Vijay <vijaytetrax@outlook.com>
2024-12-09 22:23:58 +0530

    useState Hook

--- src/Content.js
+++ src/Content.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import { useState } from "react";
 
 const Content = () => {
 
+    const [name, setName] = useState('Tetrax');
+    const [count, setCount] = useState(0);
+
     const getName = () => {
         const nameList = ['Surya', 'Pavan', 'Aswin', 'Vijay', 'Tetrax', 'Ramsy'];
-        return nameList[Math.floor(Math.random() * 6)];
-      }
+        setName(nameList[Math.floor(Math.random() * 6)]);
+    }
+
+    const handleClick = () => {
+      setCount(count+1);
+      setCount(count+1);
+      setCount(count+1);
+      console.log('currentCount: ',count); 
+
+    }
+    const handleClick2 = () => {
+      console.log('currentCount: ', count); 
+    }
       
   return (
     <main>
         <p>
-            Hello {getName()}!
+            Hello {name}!
         </p>
+        <button onClick={getName}>Change User</button>
+        <button onClick={handleClick}>Change User</button>
+        <button onClick={handleClick2}>Change User</button>
     </main>
   )
 }
