4416f1bb2b1db9c11d60323f60da48aab914ced8
Vijay <vijaytetrax@outlook.com>
2024-12-14 20:49:40 +0530

    Handling Lists & Keys

--- src/Content.js
+++ src/Content.js
@@ -1,34 +1,56 @@
 import { useState } from "react";
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
 
-    const [name, setName] = useState('Tetrax');
-    const [count, setCount] = useState(0);
+    const [people, setPeople] = useState([
+      { id: 1, name: 'Vijay', age: 30, checked: false },
+      { id: 2, name: 'Sindhu', age: 30, checked: false },
+      { id: 3, name: 'Vidhuran', age: 4, checked: false }
+    ]);
 
-    const getName = () => {
-        const nameList = ['Surya', 'Pavan', 'Aswin', 'Vijay', 'Tetrax', 'Ramsy'];
-        setName(nameList[Math.floor(Math.random() * 6)]);
+    const handleCheckBox = (id) => {
+        const peopleList = people.map((person)=> person.id === id ? {...person, checked: !person.checked} : person);
+        setPeople(peopleList);
+        localStorage.setItem('peopleList', JSON.stringify(peopleList));
+        console.table(people);
     }
 
-    const handleClick = () => {
-      setCount(count+1);
-      setCount(count+1);
-      setCount(count+1);
-      console.log('currentCount: ',count); 
-
-    }
-    const handleClick2 = () => {
-      console.log('currentCount: ', count); 
+    const handleDelete = (id) => {
+        const peopleList = people.filter((person)=> person.id != id);
+        setPeople(peopleList);
+        localStorage.setItem('peopleList', JSON.stringify(peopleList));
+        console.table(people);
     }
       
   return (
     <main>
-        <p>
-            Hello {name}!
-        </p>
-        <button onClick={getName}>Change User</button>
-        <button onClick={handleClick}>Change User</button>
-        <button onClick={handleClick2}>Change User</button>
+      {people.length ? (
+        <ul>
+            {people.map((person)=> (
+                <li className="item" key={person.id}>
+                    <input 
+                      type="checkbox"
+                      checked={person.checked}
+                      onChange={()=>handleCheckBox(person.id)}
+                    />
+                    <label 
+                      style={(person.checked) ? {textDecoration: 'line-through'} : null}
+                      onDoubleClick={()=>handleCheckBox(person.id)}
+                    > 
+                      {person.name} : {person.age}
+                    </label>
+                    <FaTrashAlt 
+                      role="button"
+                      tabIndex="0"
+                      onClick={()=>handleDelete(person.id)}
+                    />
+                </li>
+            ))}
+        </ul>
+      ) : (
+        <p style={{marginTop: '2rem'}}>The list is empty</p>
+      )}
     </main>
   )
 }
